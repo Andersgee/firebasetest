2ad2f2b26a36e657907d4e17275938eafb449695
Anders Gustafsson <andersgee@gmail.com>
2020-12-22 06:23:53 +0100

    user login

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { Counter } from "./features/counter";
-import googleauth from "./googleauth";
+import { googleauth } from "./firebase";
 
 export default function App() {
-  //const [provider, setProvider] = useState(null)
-  useEffect(() => {
-    const provider = googleauth();
-    console.log("provider: ", provider);
-  }, []);
+  const [user, setUser] = useState(null);
 
-  return (
+  return user ? (
     <>
-      {"hmm?"}
-      <Counter />
+      <div>{"you are logged in. here is your face"}</div>
+      <img src={user.photoURL} />
+      <div>{user.displayName}</div>
+      <div>{user.email}</div>
+      <div>{user.phoneNumber}</div>
+    </>
+  ) : (
+    <>
+      <div>{"your are NOT logged in"}</div>
+      <button onClick={() => googleauth(setUser)}>Login</button>
     </>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -5,25 +5,26 @@ import "firebase/auth";
 const config = {
   apiKey: "AIzaSyCNod2jvNPlE3UjmofnmurpX02_FQ9bbGY",
   authDomain: "andytest-ca4f8.web.app",
-  projectId: "andytest-ca4f8",
-  messagingSenderId: "336124957761",
+  //projectId: "andytest-ca4f8",
+  //messagingSenderId: "336124957761",
 };
 //https://firebase.google.com/docs/auth/web/google-signin?authuser=0
+
+//https://console.developers.google.com/apis/credentials?project=andytest-ca4f8
 firebase.initializeApp(config);
 
-export default function googleauth() {
+export function googleauth(setUser) {
   const provider = new firebase.auth.GoogleAuthProvider();
 
   firebase
     .auth()
     .signInWithPopup(provider)
-    .then((result) => {
-      console.log("result: ", result);
-
-      /*
-      var token = result.credential.accessToken;
-      var user = result.user;
-      */
+    .then((res) => {
+      //console.log("res: ", res);
+      //const token = res.credential.accessToken;
+      //const user = res.user;
+      //const {photoURL, displayName, email, phoneNumber} = res.user
+      setUser(res.user);
     })
     .catch((error) => {
       console.log("error: ", error);
@@ -34,6 +35,4 @@ export default function googleauth() {
       var credential = error.credential;
       */
     });
-
-  return provider;
 }

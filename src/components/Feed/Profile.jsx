10f248b910b8ed276d1ccb9839566b67cfb4e5e9
Anders Gustafsson <andersgee@gmail.com>
2020-12-23 11:53:52 +0100

    Profile fade transition

--- src/components/Feed/Profile.jsx
+++ src/components/Feed/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Box, Typography } from "@material-ui/core";
+import "./Profile.css";
 
 export default function Profile(props) {
   const [show, setShow] = useState(false);
@@ -20,29 +21,27 @@ export default function Profile(props) {
           />
         </Box>
       </Box>
-      {show && (
-        <Box
-          position="absolute"
-          borderRadius="10px"
-          bgcolor="#fffafa"
-          style={{ "z-index": "1" }}
-          py={2}
-          px={2}
-        >
-          <Typography variant="body2">
-            <Box fontWeight={400} width="240px">
-              <Box>
-                <img
-                  src={user.photoURL}
-                  alt={user.displayName}
-                  style={{ "object-fit": "contain" }}
-                />
-              </Box>
-              <Box>{user.displayName}</Box>
-            </Box>
-          </Typography>
-        </Box>
-      )}
+      <Box
+        position="absolute"
+        className={show ? "fadeIn" : "fadeOut"}
+        borderRadius="10px"
+        bgcolor="#fffafa"
+        style={{ "z-index": "1" }}
+        py={2}
+        px={2}
+      >
+        <Typography variant="body2">
+          <Box fontWeight={400} width="240px">
+            <img
+              src={user.photoURL}
+              alt={user.displayName}
+              style={{ "object-fit": "contain" }}
+            />
+
+            <Box>{user.displayName}</Box>
+          </Box>
+        </Typography>
+      </Box>
     </Box>
   );
 }

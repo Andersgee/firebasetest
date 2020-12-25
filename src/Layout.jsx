9d2f8cd3921be388d656c283b281ca07cd67c1bb
Anders Gustafsson <andersgee@gmail.com>
2020-12-25 21:36:29 +0100

    how to use array in order of pushes.. hmm

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -7,13 +7,23 @@ import { Login } from "./components/Login";
 import { Feed } from "./components/Feed";
 import { MessageBox } from "./components/MessageBox";
 
+import { storemessage } from "./firebase";
+
 export default function App() {
   const [user, setUser] = useState(null);
   const users = useUsers();
 
+  const sendmsg = () => {
+    const msg = "testing a message";
+    const uid1 = "YUYJ6XEInHUcC5OhkjPBkJ9KHnB3";
+    const uid2 = "pv4NrXOCxsM1AYaLxTOy6q0RPbq1";
+    storemessage(msg, uid1, uid2);
+  };
+
   return user ? (
     <>
       <Header />
+      <button onClick={sendmsg}>SEND MSG</button>
       <Container>
         <Grid container>
           <Grid item xs={1} md={2}></Grid>
--- src/firebase.js
+++ src/firebase.js
@@ -22,8 +22,11 @@ const config = {
   messagingSenderId: "336124957761",
   appId: "1:336124957761:web:8965ff3f00f3f8786522c0",
 };
-
 firebase.initializeApp(config);
+
+//btw this is probably how to avoid keys here... :
+//import admin from 'firebase-admin';
+//admin.initializeApp();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 
@@ -88,5 +91,35 @@ export const fetchposts = (setPosts) => () => {
     .onSnapshot((res) => {
       const posts = res.docs.map((d) => d.data());
       setPosts(posts);
+    });
+};
+
+export async function storemessage(message, uid1, uid2) {
+  let ref = firebase.firestore().doc(`messages/${uid1}-${uid2}`);
+  //const doc = await ref.get();
+  //if (doc.exists) {
+
+  //AFTER FINALLY DOING IT RIGHT, TURNS OUT CANT HAVE TIMESTAMP IN ARRAY...
+  const timestamp = "b"; //firebase.firestore.FieldValue.serverTimestamp();
+
+  const entry = {
+    messages: [{ timestamp: timestamp, message: "initial contact" }],
+  };
+  ref.set(entry);
+
+  // this is how to "push" to an array
+  // (arrays are not ordered so push an object with a timestamp aswell)
+  //const entry = { timestamp: timestamp, message: message };
+  //ref.update({ messages: firebase.firestore.FieldValue.arrayUnion(entry) });
+}
+
+export const fetchmessages = (setMessages, uid1, uid2) => () => {
+  //call with a useEffect(fetchposts(setPosts))
+  firebase
+    .firestore()
+    .collection(`messages/${uid1}-${uid2}`)
+    .onSnapshot((res) => {
+      const messages = res.docs.map((d) => d.data());
+      setMessages(messages);
     });
 };
